1a1a05844cdee460049f8b407b96f348964c7fac
Ivan Sitor <ivan.sitor1217@gmail.com>
2025-11-02 11:51:31 +0700

    add test_main.py

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ app = Flask(__name__)
 def index():
     return 'Hello, World!'
 
-def test_homepage():
-    client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
-
 def main():
     app.run(port=int(os.environ.get('PORT', 5000)))
 
