8b610a5b1cdb5c89eb49757683d07cccad4be8c3
Ivan Sitor <ivan.sitor1217@gmail.com>
2025-11-02 11:46:40 +0700

    Add test for homepage response status

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 
 from flask import Flask, send_file
+from app import app
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return 'Hello, World!'
+
+def test_homepage():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
 
 def main():
     app.run(port=int(os.environ.get('PORT', 5000)))
